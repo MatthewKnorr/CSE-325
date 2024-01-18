20e1975efaa93a3c2505212d0610ae9829fe9b50
Matthew Knorr <matt@knorrs.net>
2024-01-18 01:29:36 -0700

    Final Touch

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c1007656597c28d6e5ccf1da0935de289ff1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189b88a5b8519a2d53412d0c706c1b3dbd86ac67")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
