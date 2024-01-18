189b88a5b8519a2d53412d0c706c1b3dbd86ac67
Matthew Knorr <matt@knorrs.net>
2024-01-17 23:57:18 -0700

    MVC Rating

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c985d2fd9fccc72325f340b1616fe96d5a0e1cb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c1007656597c28d6e5ccf1da0935de289ff1ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
